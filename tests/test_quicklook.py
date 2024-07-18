2718ce08df96d371ddf295d4214cc1ff9fd27278
Carlos Gascon <carlosgascon1996@gmail.com>
2024-07-18 12:19:23 -0700

    Creating tests folder

---
+++ tests/test_quicklook.py
@@ -0,0 +1,13 @@
+
+from exotic_uvis.stage_0 import quicklookup
+
+
+
+# data directory of the form '/path to directory with flt files/'
+data_dir = '/Users/carlos/Documents/PhD/ExoTiC-HUG/DATA/WASP-178/INPUT/'
+
+# output directory of the form '/path to output directory/'
+output_dir = '/Users/carlos/Desktop/'
+
+# call quick look up function
+quicklookup(data_dir, output_dir)
