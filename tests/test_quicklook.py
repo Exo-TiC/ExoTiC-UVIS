8de87e9b73fd2f09706865bba239595f5b711794
David Grant <davidttgg@gmail.com>
2024-07-18 16:54:17 -0700

    Tests updated for latest file org.

--- tests/test_quicklook.py
+++ tests/test_quicklook.py
@@ -4,10 +4,10 @@ from exotic_uvis.stage_0 import quicklookup
 
 
 # data directory of the form '/path to directory with flt files/'
-data_dir = '/Users/carlos/Documents/PhD/ExoTiC-HUG/DATA/WASP-178/INPUT/'
+data_dir = '/Users/bp21742/research/data/hst/w31/uvis/raw2/specimages'
 
 # output directory of the form '/path to output directory/'
-output_dir = '/Users/carlos/Desktop/'
+output_dir = '/Users/bp21742/research/data/hst/w31/uvis/run_v1'
 
 # call quick look up function
 quicklookup(data_dir, output_dir)
