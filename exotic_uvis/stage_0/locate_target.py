745ecbacda5b2bd59c1cd641f3e1d5ce9cf017d5
AbbyBoehm <vab55@cornell.edu>
2024-07-19 09:10:21 -0700

    Now returns coordinates as callables

--- exotic_uvis/stage_0/locate_target.py
+++ exotic_uvis/stage_0/locate_target.py
@@ -7,14 +7,14 @@ import matplotlib.pyplot as plt
 
 def locate_target(direct_image):
     '''
-    Opens the direct image and finds the target
+    Opens the direct image and finds the target star.
     
     :param direct_image: str. Path to the direct image.
     :return: location of the direct image in x, y floats.
     '''    
     # Define three parameters for finding the target.
-    satisfied = False       # whether the user is happy with the location identifiede by DAOStarFinder
-    dist_threshold = 100    # how far to search from the initial guess
+    satisfied = False       # whether the user is happy with the location identified by DAOStarFinder
+    search_radius = 100     # how far to search from the initial guess
     threshold = 50          # how many sigma from the frame median your source is, which is quite a few
     with fits.open(direct_image) as fits_file:
         # Open the data and show it to the user.
@@ -38,7 +38,7 @@ def locate_target(direct_image):
             possible_sources = []
 
             for ind1, ind2 in zip(sources[1][:], sources[2][:]):
-                if (((ind1-bestx)**2 + (ind2-besty)**2)**0.5 < dist_threshold):
+                if (((ind1-bestx)**2 + (ind2-besty)**2)**0.5 < search_radius):
                     possible_sources.append((ind1, ind2))
             print("Located %.0f possible sources." % len(possible_sources))
             print("Please select the source from this list:")
@@ -56,17 +56,19 @@ def locate_target(direct_image):
             plt.show()
             plt.close()
 
-            check = int(input("Enter 0 to keep this source, 1 to search again,\n2 to update the threshold and then search again, or 3 to select manually: "))
+            check = int(input("Enter 0 to keep this source, 1 to search again,\n2 to update the threshold/search radius and then search again, or 3 to select manually: "))
             if check == 0:
                 satisfied = True
             if check == 2:
                 print("Current threshold: %.3f" % threshold)
                 threshold = int(input("Enter new threshold: "))
+                print("Current search radius: %.3f" % search_radius)
+                search_radius = int(input("Enter new search radius: "))
             if check == 3:
                 xs = float(input("Enter x: "))
                 ys = float(input("Enter y: "))
                 plt.subplot(1,1,1)
-                im = plt.imshow(np.log10(np.abs(d)), origin="lower", cmap="binary_r")
+                plt.imshow(d, vmin=0, vmax=100, origin='lower', cmap='binary_r')
                 plt.scatter(xs, ys, s=8, marker="x", color="red")
                 plt.xlim(int(xs-70), int(xs+70))
                 plt.ylim(int(ys-70), int(ys+70))
@@ -75,7 +77,5 @@ def locate_target(direct_image):
                 check2 = int(input("Is this okay (0) or do you want to search again (1)?: "))
                 if check2 == 0:
                     satisfied = True
-                else:
-                    pass
     print("Source selected:", xs, ys)
     return xs, ys
