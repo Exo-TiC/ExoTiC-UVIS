d2c55d66266749fb03953b247df5333b7539210f
Carlos Gascon <carlosgascon1996@gmail.com>
2024-07-18 17:04:09 -0700

    updating test quick look

--- tests/test_quicklook.py
+++ tests/test_quicklook.py
@@ -1,10 +1,8 @@
 
 from exotic_uvis.stage_0 import quicklookup
 
-
-
 # data directory of the form '/path to directory with flt files/'
-data_dir = '/Users/carlos/Documents/PhD/ExoTiC-HUG/DATA/WASP-178/INPUT/'
+data_dir = '/Users/carlos/Documents/PhD/WASP178/INPUT/'
 
 # output directory of the form '/path to output directory/'
 output_dir = '/Users/carlos/Desktop/'
