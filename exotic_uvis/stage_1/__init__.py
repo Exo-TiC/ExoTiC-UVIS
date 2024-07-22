1ab9150a688e0fc107f6ab9b55c1e99ef2c4c4c7
AbbyBoehm <vab55@cornell.edu>
2024-07-22 13:01:11 -0700

    Fixed iteration outlier rejection

--- exotic_uvis/stage_1/__init__.py
+++ exotic_uvis/stage_1/__init__.py
@@ -1,6 +1,7 @@
 __all__ = [
     "read_data",
     "laplacian_edge_detection",
+    "fixed_iteration_rejection",
     "track0th",
     "bckg_subtract"
 
@@ -10,3 +11,4 @@ from .load_data import read_data
 from .laplacian_edge_detection import laplacian_edge_detection
 from .COM_track0th import track0th
 from .bckg_subtract import Pagul_bckg_subtraction, full_frame_bckg_subtraction
+from .temporal_outlier_rejection import fixed_iteration_rejection
