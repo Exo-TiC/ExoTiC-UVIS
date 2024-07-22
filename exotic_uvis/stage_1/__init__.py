545e6e133f7f99d91c2de53a908a6ce138132833
Carlos Gascon <carlosgascon1996@gmail.com>
2024-07-22 14:23:33 -0700

    Creating displacements file, adding track bkg stars function and updating stage 1 init file

--- exotic_uvis/stage_1/__init__.py
+++ exotic_uvis/stage_1/__init__.py
@@ -2,11 +2,14 @@ __all__ = [
     "read_data",
     "laplacian_edge_detection",
     "track0th",
-    "bckg_subtract"
-
+    "Pagul_bckg_subtraction",
+    "full_frame_bckg_subtraction",
+    "corner_bkg_subtraction",
+    "track_bkgstars"
 ]
 
 from .load_data import read_data
 from .laplacian_edge_detection import laplacian_edge_detection
 from .COM_track0th import track0th
-from .bckg_subtract import Pagul_bckg_subtraction, full_frame_bckg_subtraction
+from .bckg_subtract import Pagul_bckg_subtraction, full_frame_bckg_subtraction, corner_bkg_subtraction
+from .compute_displacements import track_bkgstars
---
+++ exotic_uvis/stage_1/compute_displacements.py
@@ -0,0 +1,84 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from astropy.io import fits
+from scipy.stats import norm
+from scipy import optimize
+from photutils.centroids import centroid_com, centroid_2dg, centroid_quadratic
+
+
+def track_bkgstars(obs, bkg_stars, window = 15, plot = False, check_all = False):
+
+    """
+    
+    Function to compute the x & y displacement of a given background star
+    
+    """
+
+    # intialize and copy images
+    stars_pos, abs_pos = [], []
+    images = obs.images.data.copy()
+
+    # iterate over all listed background stars
+    for i, pos_init in enumerate(bkg_stars):
+        
+        # initialize position
+        pos = []
+
+        # get window limits
+        x0, xf = pos_init[0] - window, pos_init[0] + window
+        y0, yf = pos_init[1] - window, pos_init[1] + window
+
+        # iterate over all images
+        for image in images:
+
+            # define region around background star
+            sub_image = image[y0:yf, x0:xf]
+            
+            # compute centroid
+            x1, y1 = centroid_com(sub_image)
+
+            # append location
+            pos.append([x0 + x1, y0 + y1])
+        
+        rel_pos = np.array(pos) - pos[0]
+        
+        #if check_all:
+        #    utils.plot_image([images[0]], scatter_data = [x0 + x1, y0 + y1])
+
+        # save background star location as a function of time
+        obs["star{}_disp".format(i)] = (("exp_time", "xy"), rel_pos)
+        stars_pos.append(rel_pos)
+        abs_pos.append(pos)
+
+    stars_pos = np.array(stars_pos)
+    mean_pos = np.mean(stars_pos, axis = 0)
+
+    obs["meanstar_disp"] = (("exp_time", "xy"), mean_pos)
+    
+    # if true, plot the calculated displacements
+    if plot:
+        mean_loc = list(np.mean(abs_pos, axis = 1).transpose())
+    
+        plt.figure(figsize = (18, 5))
+        plt.imshow(np.log10(image), origin = 'lower', vmin = 0, vmax = 4, cmap = 'gist_gray')
+        plt.xlabel('Pixel')
+        plt.ylabel('Pixel')
+        plt.colorbar()
+        plt.scatter(mean_loc[0], mean_loc[1], s = 30, color = 'r', marker = '+')
+
+        plt.figure(figsize = (10, 7))
+        plt.plot(obs.exp_time.data, mean_pos[:, 0], '-o')
+        plt.plot(obs.exp_time.data, np.transpose(stars_pos[:, :, 0]), '-o', alpha = 0.5)
+        plt.xlabel('Exposure times')
+        plt.ylabel('X pixel displacement')
+
+        plt.figure(figsize = (10, 7))
+        plt.plot(obs.exp_time.data, mean_pos[:, 1], '-o')
+        plt.plot(obs.exp_time.data, np.transpose(stars_pos[:, :, 1]), '-o', alpha = 0.5)
+        plt.xlabel('Exposure times')
+        plt.ylabel('Y pixel displacement')
+        plt.show()
+
+    return pos
+
+
