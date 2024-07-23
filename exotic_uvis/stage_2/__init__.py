3cfe892a8c4e6c3c85c35db164969997d2ef8bfc
AbbyBoehm <vab55@cornell.edu>
2024-07-23 16:38:52 -0700

    Init extended to have new funcs

--- exotic_uvis/stage_2/__init__.py
+++ exotic_uvis/stage_2/__init__.py
@@ -1,5 +1,9 @@
 __all__ = [
     "get_trace_solution",
+    "standard_extraction",
+    "clean_spectra",
 ]
 
 from exotic_uvis.stage_2.trace_fitting import get_trace_solution
+from exotic_uvis.stage_2.standard_extraction import standard_extraction
+from exotic_uvis.stage_2.clean_spectra import clean_spectra
