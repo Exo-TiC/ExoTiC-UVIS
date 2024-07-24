2f9091255c62e4be85ddf163626bd372b18308c4
AbbyBoehm <vab55@cornell.edu>
2024-07-24 13:03:08 -0700

    Changes to how pipeline is operated

--- exotic_uvis/run_pipeline.py
+++ exotic_uvis/run_pipeline.py
@@ -19,85 +19,92 @@ from exotic_uvis.stage_1 import free_iteration_rejection
 from exotic_uvis.stage_1 import fixed_iteration_rejection
 from exotic_uvis.stage_1 import laplacian_edge_detection
 
-from exotic_uvis.stage_2 import load_data 
-#from exotic_uvis.stage_2 import save_data
 
 
 
-
-def run_pipeline(config_files_dir):
+def run_pipeline(config_files_dir, stages=(0,1,2,3,4,5)):
 
 
     ######## Run Stage 0 ########
-    stage0_config = glob.glob(os.path.join(config_files_dir,"stage_0*"))[0]
-    stage0_dict = parse_config(stage0_config)
+    if 0 in stages:
+        stage0_config = glob.glob(os.path.join(config_files_dir,"stage_0*"))[0]
+        stage0_dict = parse_config(stage0_config)
 
-    # run data download
-    if stage0_dict['do_download']:
-        get_files_from_mast(stage0_dict['programID'], stage0_dict['target_name'], 
-                            stage0_dict['visit_number'], stage0_dict['MASToutput_dir'], extensions=stage0_dict['extensions'])
-   
-    # collect and move files
-    if stage0_dict['do_organize']:
-        collect_and_move_files(stage0_dict['visit_number'], 
-                            stage0_dict['filesfrom_dir'], stage0_dict['filesto_dir'])
+        # run data download
+        if stage0_dict['do_download']:
+            get_files_from_mast(stage0_dict['programID'], stage0_dict['target_name'], 
+                                stage0_dict['visit_number'], stage0_dict['toplevel_dir'], extensions=stage0_dict['extensions'])
     
-    # locate target in direct image
-    if stage0_dict['do_locate']:
-        locate_target(stage0_dict['direct_image'])
+        # collect and move files
+        if stage0_dict['do_organize']:
+            if not stage0_dict['filesfrom_dir']:
+                stage0_dict['filesfrom_dir'] = stage0_dict['toplevel_dir'] # if the data weren't pre-downloaded, then they are here
+            collect_and_move_files(stage0_dict['visit_number'], 
+                                stage0_dict['filesfrom_dir'], stage0_dict['toplevel_dir'])
+        
+        # locate target in direct image
+        if stage0_dict['do_locate']:
+            locate_target(os.path.join(stage0_dict['toplevel_dir'],"directimages/or01dr001_flt.fits"))
 
-    # create quicklook gif
-    if stage0_dict['do_quicklook']:
-        quicklookup(stage0_dict['data_dir'], stage0_dict['gif_dir'])
+        # create quicklook gif
+        if stage0_dict['do_quicklook']:
+            quicklookup(stage0_dict['toplevel_dir'], stage0_dict['gif_dir'])
 
 
     ####### Run Stage 1 #######
-    stage1_config = glob.glob(os.path.join(config_files_dir,"stage_1*"))[0]
-    stage1_dict = parse_config(stage1_config)
+    if 1 in stages:
+        stage1_config = glob.glob(os.path.join(config_files_dir,"stage_1*"))[0]
+        stage1_dict = parse_config(stage1_config)
 
 
-    # read data
-    obs = read_data(stage1_dict['data_dir'], verbose = stage1_dict['verbose'])
+        # read data
+        obs = read_data(stage1_dict['toplevel_dir'], verbose = stage1_dict['verbose'])
+        # create output directory
+        output_dir_S1 = os.path.join(stage1_dict['toplevel_dir'],'outputs')
+        run_dir_S1 = os.path.join(output_dir_S1,stage1_dict['run_name'])
+        if not os.path.exists(run_dir_S1):
+            os.makedirs(run_dir_S1)
 
 
-    # temporal removal fixed iterations
-    if stage1_dict['do_fixed_iter']:
-        fixed_iteration_rejection(obs, stage1_dict['fixed_sigmas'], stage1_dict['replacement'])
-    
-    # temporal removal free iterations
-    if stage1_dict['do_free_iter']:
-        free_iteration_rejection(obs, stage1_dict['free_sigma'])
+        # temporal removal fixed iterations
+        if stage1_dict['do_fixed_iter']:
+            fixed_iteration_rejection(obs, stage1_dict['fixed_sigmas'], stage1_dict['replacement'])
+        
+        # temporal removal free iterations
+        if stage1_dict['do_free_iter']:
+            free_iteration_rejection(obs, stage1_dict['free_sigma'])
 
-    # spatial removal
-    if stage1_dict['do_led']:
-        laplacian_edge_detection(obs, 
-                                 sigma = stage1_dict['led_threshold'], 
-                                 factor = stage1_dict['led_factor'], 
-                                 n = stage1_dict['led_n'], 
-                                 build_fine_structure = stage1_dict['fine_structure'], 
-                                 contrast_factor = stage1_dict['contrast_factor'])
+        # spatial removal
+        if stage1_dict['do_led']:
+            laplacian_edge_detection(obs, 
+                                    sigma = stage1_dict['led_threshold'], 
+                                    factor = stage1_dict['led_factor'], 
+                                    n = stage1_dict['led_n'], 
+                                    build_fine_structure = stage1_dict['fine_structure'], 
+                                    contrast_factor = stage1_dict['contrast_factor'])
 
 
-    # background subtraction
-    if stage1_dict['do_mode_med']:
-        full_frame_bckg_subtraction(obs, 
-                                    bin_number = stage1_dict['bin_number'], 
-                                    fit=stage1_dict['fit'], 
-                                    value=stage1_dict['value'])
-    
-    if stage1_dict['do_corners']:
-        corner_bkg_subtraction(obs, bounds=stage1_dict['bounds'], 
-                               fit=stage1_dict['fit'])
+        # background subtraction
+        if stage1_dict['do_full_frame']:
+            full_frame_bckg_subtraction(obs, 
+                                        bin_number = stage1_dict['bin_number'], 
+                                        fit=stage1_dict['fit'], 
+                                        value=stage1_dict['value'])
+        
+        if stage1_dict['do_corners']:
+            corner_bkg_subtraction(obs, bounds=stage1_dict['bounds'], 
+                                fit=stage1_dict['fit'])
 
-    # displacements
-    if stage1_dict['do_bkg_stars']:
-        track_bkgstars(obs,  bkg_stars = stage1_dict['location'])
+        # displacements
+        if stage1_dict['do_displacements']:
+            track_bkgstars(obs,  bkg_stars = stage1_dict['location'])
 
-    # save results
-    if stage1_dict['do_save']:
-        save_data(obs, stage1_dict['output_dir'])
+        # save results
+        if stage1_dict['do_save']:
+            save_data(obs, os.path.join(run_dir_S1,'stage_1'))
         
 
-    # Run Stage 2
-    stage2_config = glob.glob(os.path.join(config_files_dir,"stage_2*"))[0]
-    stage2_dict = parse_config(stage2_config)
+    ####### Run Stage 2 #######
+    if 2 in stages:
+        stage2_config = glob.glob(os.path.join(config_files_dir,"stage_2*"))[0]
+        stage2_dict = parse_config(stage2_config)
