16ec8e9bcbeea1f0256853ad53568ff8f222a719
Abby Boehm <vab55@cornell.edu>
2024-09-30 17:13:31 -0400

    Condensed bckg methods

--- exotic_uvis/stage_1/__init__.py
+++ exotic_uvis/stage_1/__init__.py
@@ -5,8 +5,7 @@ __all__ = [
     "fixed_iteration_rejection",
     "Pagul_bckg_subtraction",
     "column_by_column_subtraction",
-    "full_frame_bckg_subtraction",
-    "corner_bkg_subtraction",
+    "uniform_value_bkg_subtraction",
     "track_bkgstars",
     "track_0thOrder",
     "plot_exposure",
@@ -18,7 +17,7 @@ __all__ = [
 from exotic_uvis.stage_1.load_and_save_data import load_data_S1
 from exotic_uvis.stage_1.load_and_save_data import save_data_S1
 from exotic_uvis.stage_1.spatial_outlier_rejection import laplacian_edge_detection, spatial_smoothing
-from exotic_uvis.stage_1.bckg_subtract import Pagul_bckg_subtraction, full_frame_bckg_subtraction, corner_bkg_subtraction, column_by_column_subtraction
+from exotic_uvis.stage_1.bckg_subtract import Pagul_bckg_subtraction, uniform_value_bkg_subtraction, column_by_column_subtraction
 from exotic_uvis.stage_1.temporal_outlier_rejection import fixed_iteration_rejection, free_iteration_rejection
 from exotic_uvis.stage_1.compute_displacements import track_bkgstars, track_0thOrder, refine_location
 from exotic_uvis.plotting.plot_exposures import plot_exposure
